6c1c324e4c113cae04b96853a6132573af8c9181
Sebastia <sguisasola@dreampulse.es>
2020-11-04 11:10:23 +0100

    Reglas 14,15,16,17, Cambios y Glosario

--- src/app/pages/temary/cambios/cambios.component.ts
+++ src/app/pages/temary/cambios/cambios.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CambiosComponent implements OnInit {
 
+  pagina: number = 0;
+
   constructor() { }
 
   ngOnInit() {}
--- src/app/pages/temary/regla15/regla15.component.ts
+++ src/app/pages/temary/regla15/regla15.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Regla15Component implements OnInit {
 
+  pagina: number = 0;
+
   constructor() { }
 
   ngOnInit() {}
